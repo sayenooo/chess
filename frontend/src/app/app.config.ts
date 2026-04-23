c83f64c763cee794c47f9b5c6a75479eee0edf25
sayenooo <zhansaya2007ke@gmail.com>
2026-04-23 06:25:23 +0500

    register

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
@@ -7,7 +7,6 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient()
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register.component';
 import { GamePageComponent } from './pages/game-page/game-page.component';
 
 export const routes: Routes = [
   { path: '', component: GamePageComponent },
-  { path: 'game', component: GamePageComponent }
+  { path: 'game', component: GamePageComponent },
+  { path: 'register', component: RegisterComponent },
+
 ];
